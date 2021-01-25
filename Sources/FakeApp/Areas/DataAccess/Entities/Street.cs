8dcfdc207ba0216194c0c6265c5f578b97a8ce22
Matthias Müller <matthias.mueller@trivadis.com>
2021-01-25 18:01:54 +0100

    WIP

--- Sources/FakeApp/Areas/DataAccess/Entities/Street.cs
+++ Sources/FakeApp/Areas/DataAccess/Entities/Street.cs
@@ -1,7 +1,9 @@
-﻿using Mmu.Mlh.EfDataAccess.Areas.Entities;
+﻿using JetBrains.Annotations;
+using Mmu.Mlh.EfDataAccess.Areas.Entities;
 
 namespace Mmu.Mlh.EfDataAccess.FakeApp.Areas.DataAccess.Entities
 {
+    [UsedImplicitly]
     public class Street : EntityBase
     {
         public string StreetName { get; set; }
